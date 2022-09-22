f5c1f5a2f69da1759bd5bc6bbe8804d96c4ef5b5
MrC0D3 <guiancam@gmail.com>
2022-09-22 18:29:11 -0300

    ajustes

--- api/__init__.py
+++ api/__init__.py
@@ -1,35 +1,19 @@
-from flask import Flask, Blueprint, request, Response
-from api.views import health
-from api import crud
+from flask import Flask, Blueprint
+from api.views import health, add_task, list
 from datetime import datetime
 
 def create_app():
     api = Blueprint('api', __name__)
     app = Flask(__name__)
 
-    crd = crud.Crud()
-
     # define api routes
     api.add_url_rule('/status', 'health', view_func=health, methods=['GET'])
 
-    api.add_url_rule('/despesas', 'list', view_func=crd.list, methods=['GET'])
+    api.add_url_rule('/despesas', 'list', view_func=list, methods=['GET'])
 
-    # api.add_url_rule('/despesas', 'insert', view_func=crd.insert, methods=['POST'])
+    api.add_url_rule('/despesas', 'add_task', view_func=add_task, methods=['POST'])
 
     app.register_blueprint(api, url_prefix='/api')
 
-    @app.route('/api/despesas', methods=['POST'])
-    def add_task():
-        try:
-            request_data = request.get_json()
-            crd.insert(request_data["valor"], request_data["data"], request_data["descricao"], request_data["tipo_pagamento"], request_data["categoria"])
-            return {
-                    'data': None, 
-                    "success": True
-                    }
-        except:
-            return {
-                    'data': None, 
-                    "success": False
-                    }
+    
     return app
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from flask import json, jsonify, request
+from flask import json, jsonify, request, Response
+from api import crud
 
 def health():
     """
@@ -8,3 +9,33 @@ def health():
         "status": "Service Running"
     }
     return jsonify(responseBody)
+
+
+def add_task():
+    try:
+        crd = crud.Crud()
+        request_data = request.get_json()
+        crd.insert(request_data["valor"], request_data["data"], request_data["descricao"], request_data["tipo_pagamento"], request_data["categoria"])
+        return {
+                'data': None, 
+                "success": True
+                }
+    except:
+        return {
+                'data': None, 
+                "success": False
+                }
+    
+def list():
+    try:
+        crd = crud.Crud()
+        dados = crd.list()
+        return {
+                'data': dados, 
+                "success": True
+                }
+    except:
+        return {
+                'data': None, 
+                "success": False
+                }
